1559065b03503b01ea2bb2dc4ea1ec56226a15c8
OmNikhil <omnikhil194@gmail.com>
2025-08-22 17:48:53 +0530

    ok

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,35 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Todo-App');
-items: any;
+items: {id:number; task: string; done: boolean}[]=[];
+newTodo: any;
+
+getTodos() {}
+
+onSubmit() {}
+
+toggleDone() {}
+
+onCheckboxChange() {}
+
+deleteTodo() {}
+
+deleteAll() {}
+
+editTodo() {}
+
+resetForm() {}
+
+exitApp() {
+  alert('Exit requested (cannot close tab from script).');
+}
 }
