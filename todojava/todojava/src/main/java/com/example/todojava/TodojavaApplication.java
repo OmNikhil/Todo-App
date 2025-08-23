68dc241a81da5076be65e1959ca0b1db5bd06b58
OmNikhil <omnikhil194@gmail.com>
2025-08-23 21:16:36 +0530

    ok

---
+++ todojava/todojava/src/main/java/com/example/todojava/TodojavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.todojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodojavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodojavaApplication.class, args);
+	}
+
+}
---
+++ todojava/todojava/src/test/java/com/example/todojava/TodojavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodojavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
